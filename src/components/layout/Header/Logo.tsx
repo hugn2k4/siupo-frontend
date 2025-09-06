84a3fc662474cc4cff80515fcfdce51de8e1c06f
Hung Le Cong <hcl2k4@gmail.com>
2025-09-06 20:59:46 +0700

    feat: cập nhật giao diện, asset và thêm AboutUs, xóa file không dùng

--- src/components/layout/Header/Logo.tsx
+++ src/components/layout/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import LogoImg from "../../../assets/images/logo.png";
 function Logo() {
   return (
-    <Link to="/" className="flex items-center w-16 h-16">
+    <Link to="/" className="flex items-center w-auto h-16 p-3">
       <img src={LogoImg} alt="Logo" className="w-full h-full object-cover" />
     </Link>
   );

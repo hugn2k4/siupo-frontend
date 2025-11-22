f9668e601d654193260c04becdd3979fa8b2164e
Hung Le Cong <hcl2k4@gmail.com>
2025-11-23 05:51:07 +0700

    feat: authentication oauth

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@
 // Cấu hình trung tâm cho toàn bộ project
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || "http://localhost:8080";
 
 export const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { GlobalProvider } from "./contexts/GlobalProvider.tsx";
-import { SnackbarProvider } from "./contexts/SnackbarContext";
 import { PreOrderProvider } from "./contexts/PreOrderContext";
+import { SnackbarProvider } from "./contexts/SnackbarContext";
 import "./index.css";
 import router from "./routers/routes.tsx";
 

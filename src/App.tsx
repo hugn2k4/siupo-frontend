db368f074c0629189b74e6b9062850dfa97377b0
Hung Le Cong <hcl2k4@gmail.com>
2025-11-26 20:42:03 +0700

    fix: scroll top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/common/ScrollToTop";
 import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Layout>
         <Outlet />
       </Layout>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,23 +1,16 @@
-import { useEffect } from "react";
-import AboutUs from "./components/AboutUs";
-import Hero from "./components/Hero";
 import { useBanners } from "../../hooks/useBanners";
+import PartnersSection from "../Menu/PartnersSection";
+import AboutUs from "./components/AboutUs";
+import CustomerReview from "./components/CustomerReview";
 import FoodCategory from "./components/FoodCategory";
-import WhyChooseUs from "./components/WhyChoseUs";
+import Hero from "./components/Hero";
+import LatestNewsBlog from "./components/LatestNewsBlog";
 import MenuSection from "./components/MenuSection";
 import TeamSection from "./components/TeamMember";
-import CustomerReview from "./components/CustomerReview";
-import LatestNewsBlog from "./components/LatestNewsBlog";
-import PartnersSection from "../Menu/PartnersSection";
+import WhyChooseUs from "./components/WhyChoseUs";
 function HomePage() {
   const { banners, loading } = useBanners("Home");
   const { banners: aboutBanners, loading: aboutLoading } = useBanners("About us");
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }, []);
 
   return (
     <>
